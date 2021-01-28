205a21cf5ae3766edcb54aa33c0ac2d778655fcb
Matthias Leitl <a.dead.trousers@gmail.com>
2021-01-28 21:45:48 +0100

    Updateing Magisk and trying to utilize adroid.mk better

--- Android.mk
+++ Android.mk
@@ -1,29 +1,33 @@
 LOCAL_PATH := $(call my-dir)
 
-# This makefile simply copies Magisk to $PRODUCT_OUT. The real magic lies in build/core/Make
-# https://github.com/Geofferey/omni_android_build/commit/8fc7d8f3cd8e0c09418047264475abc652439bdd
-# Normally one would not modify an itegral makefile to include things of this nature, 
-# but it attempts to overwrite init with symlinks to /system/bin/init, so, no choice.
-
-ifeq ($(BOARD_MAGISK_INIT),true)
-
 include $(CLEAR_VARS)
-LOCAL_MODULE       := magiskinit
+LOCAL_MODULE       := Magisk
 LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := FAKE
 
 ifndef $(MAGISK_VERSION)
-MAGISK_VERSION := 21.1
+MAGISK_VERSION := 21.4
 endif
+MAGISK_ARCHIVE := Magisk-v$(MAGISK_VERSION).zip
+
+LOCAL_SRC_FILES    := $(MAGISK_ARCHIVE)
 
 ifeq ($(TARGET_ARCH), arm)
-$(shell unzip -p $(LOCAL_PATH)/Magisk-v$(MAGISK_VERSION).zip arm/magiskinit > $(PRODUCT_OUT)/magiskinit)
+LOCAL_POST_INSTALL_CMD := unzip -p Magisk arm/magiskinit > magiskinit; \
+  unzip -p Magisk arm/magiskboot > magiskboot;
 else ifeq ($(TARGET_ARCH), arm64)
-$(shell unzip -p $(LOCAL_PATH)/Magisk-v$(MAGISK_VERSION).zip arm/magiskinit64 > $(PRODUCT_OUT)/magiskinit)
+LOCAL_POST_INSTALL_CMD := unzip -p Magisk arm/magiskinit64 > magiskinit; \
+  unzip -p Magisk arm/magiskboot > magiskboot;
 else ifeq ($(TARGET_ARCH), x86)
-$(shell unzip -p $(LOCAL_PATH)/Magisk-v$(MAGISK_VERSION).zip x86/magiskinit > $(PRODUCT_OUT)/magiskinit)
+LOCAL_POST_INSTALL_CMD := unzip -p Magisk x86/magiskinit > magiskinit; \
+  unzip -p Magisk x86/magiskboot > magiskboot;
 else ifeq ($(TARGET_ARCH), x86_64)
-$(shell unzip -p $(LOCAL_PATH)/Magisk-v$(MAGISK_VERSION).zip x86/magiskinit64 > $(PRODUCT_OUT)/magiskinit)
+LOCAL_POST_INSTALL_CMD := unzip -p Magisk x86/magiskinit64 > magiskinit; \
+  unzip -p Magisk x86/magiskboot > magiskboot;
 endif
 
-endif
-
+LOCAL_POST_INSTALL_CMD += unzip -p Magisk common/addon.d.sh > $(TARGET_COPY_OUT_SYSTEM)/addon.d/99-magisk.sh; \
+  unzip -p Magisk common/boot_patch.sh > boot_patch.sh; \
+  unzip -p Magisk common/util_functions.sh > util_functions.sh; \
+  boot_patch.sh $(PRODUCT_OUT)/boot.img
+include $(BUILD_PREBUILT)
