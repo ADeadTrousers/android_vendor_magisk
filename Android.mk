e403e7e7e16856534fa8bbba10add3c0bacf1bbb
Matthias Leitl <a.dead.trousers@gmail.com>
2021-01-29 22:55:11 +0100

    New approach without modifying the build process

--- Android.mk
+++ Android.mk
@@ -1,33 +1,79 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := Magisk
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE := Magisk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := $(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
 
 ifndef $(MAGISK_VERSION)
-MAGISK_VERSION := 21.4
+  MAGISK_VERSION := 21.4
 endif
-MAGISK_ARCHIVE := Magisk-v$(MAGISK_VERSION).zip
 
-LOCAL_SRC_FILES    := $(MAGISK_ARCHIVE)
+MY_MAGISK_INTERMEDIATES := $(TARGET_OUT_INTERMEDIATES)/$(LOCAL_MODULE_CLASS)/$(LOCAL_MODULE)_intermediates
+MY_MAGISK_ARCHIVE := $(LOCAL_PATH)/$(LOCAL_MODULE)-v$(MAGISK_VERSION).zip
 
+MY_MAGISK_SOURCE_IMAGE := $(MY_MAGISK_INTERMEDIATES)/new-boot.img
+MY_MAGISK_SOURCE_MANAGER := common/magisk.apk
+MY_MAGISK_SOURCE_ADDON := common/addon.d.sh
+MY_MAGISK_SOURCE_PATCH := common/boot_patch.sh
+MY_MAGISK_SOURCE_FUNCTIONS := common/util_functions.sh
 ifeq ($(TARGET_ARCH), arm)
-LOCAL_POST_INSTALL_CMD := unzip -p Magisk arm/magiskinit > magiskinit; \
-  unzip -p Magisk arm/magiskboot > magiskboot;
+  MY_MAGISK_SOURCE_INIT := arm/magiskinit
 else ifeq ($(TARGET_ARCH), arm64)
-LOCAL_POST_INSTALL_CMD := unzip -p Magisk arm/magiskinit64 > magiskinit; \
-  unzip -p Magisk arm/magiskboot > magiskboot;
+  MY_MAGISK_SOURCE_INIT := arm/magiskinit64
 else ifeq ($(TARGET_ARCH), x86)
-LOCAL_POST_INSTALL_CMD := unzip -p Magisk x86/magiskinit > magiskinit; \
-  unzip -p Magisk x86/magiskboot > magiskboot;
+  MY_MAGISK_SOURCE_INIT := x86/magiskinit
 else ifeq ($(TARGET_ARCH), x86_64)
-LOCAL_POST_INSTALL_CMD := unzip -p Magisk x86/magiskinit64 > magiskinit; \
-  unzip -p Magisk x86/magiskboot > magiskboot;
+  MY_MAGISK_SOURCE_INIT := x86/magiskinit64
+else
+  $(error Target architecture not supported: $(TARGET_ARCH))
 endif
 
-LOCAL_POST_INSTALL_CMD += unzip -p Magisk common/addon.d.sh > $(TARGET_COPY_OUT_SYSTEM)/addon.d/99-magisk.sh; \
-  unzip -p Magisk common/boot_patch.sh > boot_patch.sh; \
-  unzip -p Magisk common/util_functions.sh > util_functions.sh; \
-  boot_patch.sh $(PRODUCT_OUT)/boot.img
+ifeq ($(HOST_ARCH), arm)
+  MY_MAGISK_SOURCE_HOST := arm/magiskinit
+  MY_MAGISK_SOURCE_BOOT := arm/magiskboot
+else ifeq ($(HOST_ARCH), arm64)
+  MY_MAGISK_SOURCE_HOST := arm/magiskinit64
+  MY_MAGISK_SOURCE_BOOT := arm/magiskboot
+else ifeq ($(HOST_ARCH), x86)
+  MY_MAGISK_SOURCE_HOST := x86/magiskinit
+  MY_MAGISK_SOURCE_BOOT := x86/magiskboot
+else ifeq ($(HOST_ARCH), x86_64)
+  MY_MAGISK_SOURCE_HOST := x86/magiskinit64
+  MY_MAGISK_SOURCE_BOOT := x86/magiskboot
+else
+  $(error Host architecture not supported: $(TARGET_ARCH))
+endif
+
+MY_MAGISK_TARGET_IMAGE := $(PRODUCT_OUT)/boot.img
+MY_MAGISK_TARGET_MANAGER := $(LOCAL_PATH)/$(LOCAL_SRC_FILES)
+MY_MAGISK_TARGET_ADDON := $(PRODUCT_OUT)/system/addon.d/99-magisk.sh
+MY_MAGISK_TARGET_PATCH := $(MY_MAGISK_INTERMEDIATES)/boot_patch.sh
+MY_MAGISK_TARGET_FUNCTIONS := $(MY_MAGISK_INTERMEDIATES)/util_functions.sh
+MY_MAGISK_TARGET_BOOT := $(MY_MAGISK_INTERMEDIATES)/magiskboot
+MY_MAGISK_TARGET_INIT := $(MY_MAGISK_INTERMEDIATES)/magiskinit
+MY_MAGISK_TARGET_MAGISK := $(MY_MAGISK_INTERMEDIATES)/magisk
+
+$(shell unzip -p $(MY_MAGISK_ARCHIVE) $(MY_MAGISK_SOURCE_MANAGER) > $(MY_MAGISK_TARGET_MANAGER) 2>/dev/null)
+
+LOCAL_POST_INSTALL_CMD := \
+  rm -f $(MY_MAGISK_TARGET_MANAGER) && \
+  unzip -p $(MY_MAGISK_ARCHIVE) $(MY_MAGISK_SOURCE_ADDON) > $(MY_MAGISK_TARGET_ADDON) && \
+  unzip -p $(MY_MAGISK_ARCHIVE) $(MY_MAGISK_SOURCE_PATCH) > $(MY_MAGISK_TARGET_PATCH); \
+  unzip -p $(MY_MAGISK_ARCHIVE) $(MY_MAGISK_SOURCE_FUNCTIONS) > $(MY_MAGISK_TARGET_FUNCTIONS) && \
+  unzip -p $(MY_MAGISK_ARCHIVE) $(MY_MAGISK_SOURCE_BOOT) > $(MY_MAGISK_TARGET_BOOT) && \
+  unzip -p $(MY_MAGISK_ARCHIVE) $(MY_MAGISK_SOURCE_HOST) > $(MY_MAGISK_TARGET_INIT) && \
+  chmod 755 $(MY_MAGISK_TARGET_INIT) && \
+  $(MY_MAGISK_TARGET_INIT) -x magisk $(MY_MAGISK_TARGET_MAGISK) && \
+  unzip -p $(MY_MAGISK_ARCHIVE) $(MY_MAGISK_SOURCE_INIT) > $(MY_MAGISK_TARGET_INIT) && \
+  sed -i '1d' $(MY_MAGISK_TARGET_PATCH) && \
+  chmod 755 $(MY_MAGISK_TARGET_ADDON) && \
+  chmod 755 $(MY_MAGISK_TARGET_PATCH) && \
+  export OUTFD="1" && \
+  $(MY_MAGISK_TARGET_PATCH) ../../../boot.img && \
+  mv -f $(MY_MAGISK_SOURCE_IMAGE) $(MY_MAGISK_TARGET_IMAGE)
+
 include $(BUILD_PREBUILT)
--- README.md
+++ README.md
@@ -1,11 +1,30 @@
-# android_vendor_magisk
+# AOSP Integration for Magisk
+:warning: THIS REPOSITORY IS NOT PRODUCTION READY :warning: 
 
-This repo only copies magisinit to PRODUCT_OUT
-Additional changes to build/core/Make are required
+## Introduction
 
-~~https://github.com/Geofferey/omni_android_build/commit/8fc7d8f3cd8e0c09418047264475abc652439bdd~~
-https://github.com/ADeadTrousers/android_device_Unihertz_Atom_XL/blob/master/patch/build_make/0002-build-add-rule-to-include-Magisk-in-builds.patch
+Magisk is a suite of open source tools for customizing Android, supporting devices higher than Android 4.2. It covers fundamental parts of Android customization: root, boot scripts, SELinux patches, AVB2.0 / dm-verity / forceencrypt removals etc.
 
-This is a fork of Geofferey's original repository but tries to offer the latest Magisk version.
-For convinience it also uses the original Magisk-release zip-files instead of the unpacked files itself.
-This way it's easier for the common device maintainer to update their included Magisk version.
+It was created by and is maintained by John Wo.
+For the latest release always check [the GitHub Releases of Magisk](https://github.com/topjohnwu/Magisk/releases)
+
+The aim of this repository is simply to integrate Magisk into the build process of AOSP.
+
+## Workflow
+
+1. Extract `magisk.apk` from the archive. This is needed to have a source file the AOSP build process can use.
+2. Everything else needs to be performed at the end of the build process (Work in progress)
+3. Extract the update survival script `addon.d.sh` as `99-magisk.sh` into the `/system/addon.d` folder and set the permissions to `755`.
+4. Extract `magiskboot` and `magiskinit` for the host architecture into the `Magik_intermediates` folder and set the permissions to `755`. These are the filese needed for modifying the boot image.
+5. Run `magiskinit -x magisk magisk` to extract the magisk binary from the init command.
+6. Extract `magiskinit` for the target architecture into the `Magik_intermediates` folder. This is the one that is actually put into the boot image.
+7. Extract `boot_patch.sh` and `util_functions.sh` into the `Magik_intermediates` folder and set the permissions to `755`. These are the script files that will modify the boot image.
+8. The shebang (#!) from `boot_patch.sh` needs to be remove otherwise the script won't run.
+9. Also the script needs the correct output device so `export OUTFD="1"` is being set.
+10. Now the script can be executed with `boot_patch.sh boot.img`.
+11. The file `new-boot.img` is being created and then copied back as `boot.img` into the out directory.
+
+## Special Thanks To
+
+- [John Wo](https://github.com/topjohnwu) for creating Magisk.
+- [Geofferey Eakins](https://github.com/Geofferey) whom I got this idea from.
